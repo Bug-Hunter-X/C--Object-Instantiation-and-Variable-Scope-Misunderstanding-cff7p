71e73866ef740004cc7018bf7e95642812161d71
BugHunter-X <askzenai@gmail.com>
2025-01-28 06:04:06 +0100

    Add bug.cs file

---
+++ bug.cs
@@ -0,0 +1,18 @@
+public class MyClass
+{
+    public int MyProperty { get; set; }
+
+    public MyClass(int value)
+    {
+        MyProperty = value;
+    }
+
+    public static void Main(string[] args)
+    {
+        MyClass instance = new MyClass(10);
+        Console.WriteLine(instance.MyProperty); // Outputs 10
+
+        MyClass instance2 = new MyClass(20);
+        Console.WriteLine(instance.MyProperty); // Outputs 10, not 20. Problem here.
+    }
+}
